c3a22e86b598d2399ff89071009a59a528a2d047
Szikszai Gusztáv <guszti5@hotmail.com>
2014-05-12 10:48:16 +0200

    Dimensions spec.

--- opal/fron/dom/modules/dimensions.rb
+++ opal/fron/dom/modules/dimensions.rb
@@ -24,7 +24,6 @@ module DOM
     end
 
     def height
-
       clientRect[:height]
     end
 
---
+++ spec/dom/modules/dimensions_spec.rb
@@ -0,0 +1,55 @@
+require 'fron/dom'
+
+describe DOM::Dimensions do
+
+  let(:element) { DOM::Element.new 'div' }
+
+  before do
+    element >> DOM::Document.body
+    element.style.position = 'absolute'
+    element.style.width = '100px'
+    element.style.top = '40px'
+    element.style.left = '60px'
+    element.style.height = '80px'
+  end
+
+  after do
+    element.remove!
+  end
+
+  describe '#top' do
+    it 'should return the top position of the element' do
+      element.top.should eq 40
+    end
+  end
+
+  describe '#left' do
+    it 'should return the left position of the element' do
+      element.left.should eq 60
+    end
+  end
+
+  describe '#right' do
+    it 'should return the right position of the element' do
+      element.right.should eq 160
+    end
+  end
+
+  describe '#bottom' do
+    it 'should return the bottom position of the element' do
+      element.bottom.should eq 120
+    end
+  end
+
+  describe '#width' do
+    it 'should return the width of the element' do
+      element.width.should eq 100
+    end
+  end
+
+  describe '#height' do
+    it 'should return the height of the element' do
+      element.height.should eq 80
+    end
+  end
+end
