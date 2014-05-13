70eb6de6155d4810cc733f6f68c77b7b709e7315
Szikszai Gusztáv <guszti5@hotmail.com>
2014-05-13 14:03:49 +0200

    Fix dimensions for FF.

--- opal/fron/dom/modules/dimensions.rb
+++ opal/fron/dom/modules/dimensions.rb
@@ -4,33 +4,33 @@ module DOM
   module Dimensions
 
     def top
-      clientRect[:top] + Window.scrollY
+      `#{clientRect}.top` + Window.scrollY
     end
 
     def left
-      clientRect[:left] + Window.scrollX
+      `#{clientRect}.left` + Window.scrollX
     end
 
     def right
-      clientRect[:right] + Window.scrollX
+      `#{clientRect}.right` + Window.scrollX
     end
 
     def bottom
-      clientRect[:bottom] + Window.scrollY
+      `#{clientRect}.bottom` + Window.scrollY
     end
 
     def width
-      clientRect[:width]
+      `#{clientRect}.width`
     end
 
     def height
-      clientRect[:height]
+      `#{clientRect}.height`
     end
 
     private
 
     def clientRect
-      Hash.new `JSON.parse(JSON.stringify(#{@el}.getBoundingClientRect()))`
+      `#{@el}.getBoundingClientRect()`
     end
   end
 end
